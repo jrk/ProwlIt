9c16d3ced9bbc8995b0bc84767cfc33a2fdded0f
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2010-05-23 18:11:06 -0700

    initial import

---
+++ prowl.js
@@ -0,0 +1,17 @@
+// Method from http://www.openjs.com/articles/ajax_xmlhttp_using_post.php
+
+var url = "get_data.php";
+var params = "lorem=ipsum&name=binny";
+http.open("POST", url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+http.setRequestHeader("Content-length", params.length);
+http.setRequestHeader("Connection", "close");
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+	if(http.readyState == 4 && http.status == 200) {
+		alert(http.responseText);
+	}
+}
+http.send(params);
