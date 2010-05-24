b2c8e3787162ebbf31d53fb9f16bbcef6f4b3961
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2010-05-23 18:24:18 -0700

    updated

--- prowl.js
+++ prowl.js
@@ -1,22 +1,23 @@
 // Method from http://www.openjs.com/articles/ajax_xmlhttp_using_post.php
 
+var http = new XMLHttpRequest();
 var url = "https://prowl.weks.net/publicapi/add";
 var apikey = "XXXX";
 var application = "the web";
 var evt = "Browser!";
 var description = encodeURIComponent(document.title.replace(/^\s*|\s*$/g,''))
-                + "\n" + encodeURIComponent(location.href)
+                + escape("\n") + encodeURIComponent(location.href)
 var params = "apikey="+apikey
             +"&application="+escape(application)
             +"&event="+escape(evt)
-            +"&description="+escape(evt)
+            +"&description="+description
             ;
 http.open("POST", url, true);
 
 //Send the proper header information along with the request
 http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-http.setRequestHeader("Content-length", params.length);
-http.setRequestHeader("Connection", "close");
+//http.setRequestHeader("Content-length", params.length);
+//http.setRequestHeader("Connection", "close");
 
 http.onreadystatechange = function() {//Call a function when the state changes.
 	if(http.readyState == 4 && http.status == 200) {
