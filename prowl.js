f261bf9d8e4a2dec6d6fd96fe02ed8f57866a475
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2010-05-23 18:31:22 -0700

    working

--- prowl.js
+++ prowl.js
@@ -2,11 +2,11 @@
 
 var http = new XMLHttpRequest();
 var url = "https://prowl.weks.net/publicapi/add";
-var apikey = "XXXX";
+var apikey = "26b5dcf1c740c2ad60163f34be948db63d42d622";
 var application = "the web";
 var evt = "Browser!";
 var description = encodeURIComponent(document.title.replace(/^\s*|\s*$/g,''))
-                + escape("\n") + encodeURIComponent(location.href)
+                + escape("\n") + encodeURIComponent(location.href);
 var params = "apikey="+apikey
             +"&application="+escape(application)
             +"&event="+escape(evt)
@@ -19,9 +19,9 @@ http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 //http.setRequestHeader("Content-length", params.length);
 //http.setRequestHeader("Connection", "close");
 
-http.onreadystatechange = function() {//Call a function when the state changes.
-	if(http.readyState == 4 && http.status == 200) {
-		alert(http.responseText);
-	}
-}
+//http.onreadystatechange = function() {//Call a function when the state changes.
+//	if(http.readyState == 4 && http.status == 200) {
+//		alert(http.responseText);
+//	}
+//}
 http.send(params);
