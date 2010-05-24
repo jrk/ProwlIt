095c08900322397ae62d4ad487a81db7fed24e25
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2010-05-23 18:16:37 -0700

    initial implementation

--- prowl.js
+++ prowl.js
@@ -1,7 +1,16 @@
 // Method from http://www.openjs.com/articles/ajax_xmlhttp_using_post.php
 
-var url = "get_data.php";
-var params = "lorem=ipsum&name=binny";
+var url = "https://prowl.weks.net/publicapi/add";
+var apikey = "XXXX";
+var application = "the web";
+var evt = "Browser!";
+var description = encodeURIComponent(document.title.replace(/^\s*|\s*$/g,''))
+                + "\n" + encodeURIComponent(location.href)
+var params = "apikey="+apikey
+            +"&application="+escape(application)
+            +"&event="+escape(evt)
+            +"&description="+escape(evt)
+            ;
 http.open("POST", url, true);
 
 //Send the proper header information along with the request
